327738395f1b6fac2989dda0fbf735f85aa279ba
StrScr <andres.mejia@unitec.edu>
2017-05-24 16:54:08 -0600

    Fixed FAT generator. :D

--- fatgen.cpp
+++ fatgen.cpp
@@ -1,5 +1,6 @@
 // Generador de Archivos FAT16
 #define usint unsigned short int
+#define rechcast reinterpret_cast<const char *> 
 #include <iostream>
 #include <fstream>
 using namespace std;
@@ -16,32 +17,46 @@ int main(int argc, char* argv[]){
         return 1;
     }
     cout << "Writing Reserved Region... (512 Bytes)" << endl;
-    FATfile.write(0,3); // Bootstrap Jump Code, Unused here
+    char* buffer = new char[448];
+    for(int i = 0; i < 448;i++)
+        buffer[i] = 0;
+    /* BOOT START BLOCK */ 
+    FATfile.write(buffer,3); // Bootstrap Jump Code, Unused here
     FATfile.write("MyFS PWN",8); // OEM Name, 8 char, Doesn't really matter
     /* BIOS PARAMETER BLOCK */
-    FATfile.write((usint)512,2); //Bytes Per Sector
-    FATfile.write(8,1); // Sectors Per Cluster, 8 Sectors * 512 Bytes/Sector = 4kB Clusters
-    FATfile.write((usint)1,2); // # Reserved Sectors, FAT16 uses 1
-    FATfile.write((char)2,1); // # FAT Copies, Using 2
-    FATfile.write((usint)512,2); // # Root Dir Entries, Using 512
-    FATfile.write(0,2); // # Sectors in FS (Small), Unused here
-    FATfile.write((char)0xF8,1); // Media Descriptor, Using Fixed Disk
-    FATfile.write((usint)256,2); // # Sectors per FAT, 256 Sectors/FAT * 512 Bytes/Sector = 128kB per FAT
-    FATfile.write(0,2); // # Sectors per Track, Unused here
-    FATfile.write(0,2); // # Heads, Unused here
-    FATfile.write(0,4); // # Hidden Sectors, Unused here
-    FATfile.write(524288,4); // # Sectors in FS (Big), 524288 Sectors * 512 Bytes/Sector = 256MB FS
+    int values = 512;
+    FATfile.write(rechcast(&values),2); //Bytes Per Sector
+    values = 8;
+    FATfile.write(rechcast(&values),1); // Sectors Per Cluster, 8 Sectors * 512 Bytes/Sector = 4kB Clusters
+    values = 1;
+    FATfile.write(rechcast(&values),2); // # Reserved Sectors, FAT16 uses 1
+    values = 2;
+    FATfile.write(rechcast(&values),1); // # FAT Copies, Using 2
+    values = 512;
+    FATfile.write(rechcast(&values),2); // # Root Dir Entries, Using 512
+    FATfile.write(buffer,2); // # Sectors in FS (Small), Unused here
+    values = 0xF8;
+    FATfile.write(rechcast(&values),1); // Media Descriptor, Using Fixed Disk
+    values = 256;
+    FATfile.write(rechcast(&values),2); // # Sectors per FAT, 256 Sectors/FAT * 512 Bytes/Sector = 128kB per FAT
+    FATfile.write(buffer,2); // # Sectors per Track, Unused here
+    FATfile.write(buffer,2); // # Heads, Unused here
+    FATfile.write(buffer,4); // # Hidden Sectors, Unused here
+    values = 524288;
+    FATfile.write(rechcast(&values),4); // # Sectors in FS (Big), 524288 Sectors * 512 Bytes/Sector = 256MB FS
     /* EXTENDED BIOS PARAMETER BLOCK */
-    FATfile.write(0,1); // Drive Number, Unused
-    FATfile.write(0,1); // Reserved
-    FATfile.write(0,1); // Extended Boot Signature, Unused
-    FATfile.write(0,4); // Volume Serial Number, Unused
+    FATfile.write(buffer,1); // Drive Number, Unused
+    FATfile.write(buffer,1); // Reserved
+    FATfile.write(buffer,1); // Extended Boot Signature, Unused
+    FATfile.write(buffer,4); // Volume Serial Number, Unused
     FATfile.write("MyFATVolume",11); // Volume Label, Should be same as ROOT
     FATfile.write("'FAT16 '",8); // FS Type
     /* BOOTSTRAP CODE */
-    FATfile.write(0,448); // Bootstrap Code, Unused
-    FATfile.write((usint)0xAA55,2); // Boot Sector Signature
+    FATfile.write(buffer,448); // Bootstrap Code, Unused
+    values = 0xAA55;
+    FATfile.write(rechcast(&values),2); // Boot Sector Signature
     cout << "Done writing Reserved Region." << endl;
+    delete[] buffer;
     FATfile.close();
     return 0;
 }
