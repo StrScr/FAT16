2c4da88fc17fd37270e465b736b85ef2304d714e
StrScr <andres.mejia@unitec.edu>
2017-05-29 22:18:34 -0600

    Started with API. (Broken)

--- shell.cpp
+++ shell.cpp
@@ -1,27 +1,87 @@
+#define usint unsigned short int
+#define uncast reinterpret_cast<unsigned short int>
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
+
+#define rechcast reinterpret_cast<const char *> 
+#define reconstchcast reinterpret_cast<const char *> 
+
 using namespace std;
 
+usint getFATindex(int);
+vector<string> getTokens(string, char);
+
+const int FAToffset = 512;
+const usint FAT_EOF = 0xFFFF;
+
+fstream FAT;
+string CURR_DIR;
+
+
 int main(int argc, char* argv[]){
-    fstream file;
     if(argc > 1){
-        file.open(argv[1], ios::binary | ios::in);
+        FAT.open(argv[1], ios::binary | ios::in);
     }else{
         cout << "Please include filename in args" << endl;
         return 0;
     }
-    if(!file.is_open()){
+    if(!FAT.is_open()){
         cout << "Can't open file!" << endl;
         return 1;
     }
-    cout << "file opened succesfully" << endl;
+    cout << "File opened succesfully." << endl;
     cout << "READING BOOT SECTOR" << endl;
     char* OS_name = new char[8];
     cout << "Ignoring first 3 bytes" << endl;
-    file.seekg(file.beg+3);
-    file.read(OS_name,8);
+    FAT.seekg(FAT.beg+3);
+    FAT.read(OS_name,8);
     cout << "OS NAME: " << OS_name << endl;
     delete[] OS_name;
-    file.close();
+    /* Check FS Initalized status */
+    if (getFATindex(2)==0){
+        cout << "FAT still uninitalized. Writing root..." << endl;
+    }else{
+        cout << "Root value: " << getFATindex(2) << endl;
+    }
+    //Welcome to the shell
+    int status = 0;
+	while(!status) {
+		cout << OS_name << "> ";
+		string line;
+		getline(cin,line);
+		vector<string> tokens = getTokens(line, ' ');
+        if(tokens.size() > 0)
+            cout << "cmd to execute: " << tokens[0] << endl;
+        if(tokens.size() == 0 || tokens[0] == "exit")
+            status = 1;
+		//status = executeCommand(tokens);
+	};
+    if(status != 1){
+		cerr << "ERROR " << status << ": al ejecutar el comando" << endl;
+	}
+    FAT.close();
     return 0;
+}
+
+usint getFATindex(int index){
+    FAT.seekg(FAT.beg+FAToffset+(index*2));
+    char* value = new char[2];
+    FAT.read(value,2);
+    return uncast(value);
+}
+
+//Only works with one char delimiter but it works!!
+vector<string> getTokens(string toTokenize, char delimiter){
+	vector<string> v;
+	string::iterator stringIT = toTokenize.begin();
+	for(string::iterator i = toTokenize.begin(); i != toTokenize.end(); i++){
+		if(*i == delimiter){
+			v.push_back(string(stringIT,i));
+			stringIT=i+1;
+		}
+	}
+	v.push_back(string(stringIT,toTokenize.end()));
+	return v;
 }
