27970de6c22178f1381e4211854c238d30cdf6a7
StrScr <andres.mejia@unitec.edu>
2017-05-30 19:44:08 -0600

    Developed more API. Yet to test.

--- shell.cpp
+++ shell.cpp
@@ -12,7 +12,7 @@ using namespace std;
 struct DirEntry{
     char[11] filename;
     char attributes;
-    long created_time;
+    unsigned long created_time;
     usint address;
     unsigned int filesize;
     char[6] reserved; 
@@ -22,6 +22,11 @@ usint getFATindex(int);
 void setFATindex(int, usint);
 char* getDataCluster(int);
 void setDataCluster(int,char*);
+bool hasNextCluster(int);
+DirEntry makeDirEntry(char*,char,usint,unsigned int);
+char* getDirRawData(int, char*);
+DirEntry* parseDirEntries(char*);
+char* packDirEntries(DirEntry*);
 vector<string> getTokens(string, char);
 
 const int FAT_OFFSET = 512; // 1 sector
@@ -51,12 +56,17 @@ int main(int argc, char* argv[]){
     cout << "OS NAME: " << OS_name << endl;
     /* Check FS Initalized status */
     cout << "Checking FS status..." << endl;
+    cout << "FAT index 0: " << getFATindex(0) << endl;
+    cout << "FAT index 1: " << getFATindex(1) << endl;
+    cout << "FAT index 2 (ROOT): " << getFATindex(2) << endl;
     if (getFATindex(2)==0){
         cout << "FAT still uninitalized. Writing root..." << endl;
-    }else{
-        cout << getFATindex(0) << endl;
-        cout << getFATindex(1) << endl;
-        cout << "Root value: " << getFATindex(2) << endl;
+        DirEntry* root = parseDirEntries(getDataCluster(2));
+        root[0]=makeDirEntry(".",0x10,2,0);
+        root[1]=makeDirEntry("..",0x10,2,0);
+        setDataCluster(2,packDirEntries(root));
+        setFATindex(2,FAT_EOF);
+        cout << "Root written."
     }
     //Welcome to the shell
     int status = 0;
@@ -93,7 +103,7 @@ void setFATindex(int index, usint value){
 
 char* getDataCluster(int index){
     FAT.seekg(FAT.beg+DATA_OFFSET+(index*CLUSTER_SIZE));
-    char* data[CLUSTER_SIZE];
+    char* data = new char[CLUSTER_SIZE];
     FAT.read(data,CLUSTER_SIZE);
     return data;
 }
@@ -107,19 +117,62 @@ bool hasNextCluster(int index){
     return getFATindex(index)!=FAT_EOF; 
 }
 
-char* getDirRawData(int index){
-
+DirEntry makeDirEntry(char* fn, char attr, usint addr, unsigned int size){
+    DirEntry entry;
+    entry.filename=fn;
+    entry.attributes=attr;
+    entry.created_time=chrono::system_clock::now().time_since_epoch() / chrono::milliseconds(1);
+    entry.address=addr;
+    entry.filesize=size;
+    return entry;
 }
 
+char* getDirRawData(int index, char* data){
+    char* entry = new char[32];
+    for(int i=0; i<32; i++){
+        entry[i]=data[index*32+i];
+    }
+    return entry;
+}
+
+/**
+    Transforms a 4kB cluster of data into 512 DirEntries.
+
+    @param data The 4kB cluster as a char array.
+    @return The array of 512 DirEntries.
+*/
 DirEntry* parseDirEntries(char* data){
     DirEntry* entries = new DirEntry[512];
     for(int i=0; i<512; i++){
-        DirEntry[i] = reinterpret_cast<DirEntry>getDirRawData(i);
+        DirEntry[i] = reinterpret_cast<DirEntry>(getDirRawData(i));
     }
     return entries;
 }
 
-//Only works with one char delimiter but it works!!
+/**
+    The reverse of parseDirEntries. It packs 512 DirEntries into a 4kB cluster.
+
+    @param entries Array of 512 DirEntries to pack.
+    @return The 4kB cluster as a char array.
+*/
+char* packDirEntries(DirEntry* entries){
+    char* data = new char[CLUSTER_SIZE];
+    char* entry;
+    for(i=0; i<512; i++){
+        entry=recast(&(entries[i]));
+        for(j=0; j<32; j++){
+            data[i*32+j]=entry[j];
+        }
+    }
+    return data;
+}
+
+/**
+    Only works with one char delimiter but it works!!
+
+    @param entries Array of 512 DirEntries to pack.
+    @return The 4kB cluster as a char array.
+*/
 vector<string> getTokens(string toTokenize, char delimiter){
 	vector<string> v;
 	string::iterator stringIT = toTokenize.begin();
