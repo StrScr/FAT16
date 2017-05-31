c3b08eff5818bda0d022b77e3e3344426132493f
StrScr <andres.mejia@unitec.edu>
2017-05-30 18:07:47 -0600

    Started on FAT API.

--- shell.cpp
+++ shell.cpp
@@ -1,20 +1,32 @@
 #define usint unsigned short int
 #define uncast reinterpret_cast<unsigned short int>
 #define recast reinterpret_cast<char *>
+#define rechcast reinterpret_cast<const char *> 
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
 
-#define rechcast reinterpret_cast<const char *> 
-#define reconstchcast reinterpret_cast<const char *> 
-
 using namespace std;
 
+struct DirEntry{
+    char[11] filename;
+    char attributes;
+    long created_time;
+    usint address;
+    unsigned int filesize;
+    char[6] reserved; 
+}
+
 usint getFATindex(int);
+void setFATindex(int, usint);
+char* getDataCluster(int);
+void setDataCluster(int,char*);
 vector<string> getTokens(string, char);
 
-const int FAToffset = 512;
+const int FAT_OFFSET = 512; // 1 sector
+const int DATA_OFFSET = FAT_OFFSET + 256*1024; // 1 sector + 256kB
+const int CLUSTER_SIZE = 512*8; // 4kB (8 sectors/cluster at 512B/sector) 
 const usint FAT_EOF = 0xFFFF;
 
 fstream FAT;
@@ -34,11 +46,9 @@ int main(int argc, char* argv[]){
     cout << "File opened succesfully." << endl;
     cout << "READING BOOT SECTOR" << endl;
     char* OS_name = new char[8];
-    cout << "Ignoring first 3 bytes" << endl;
     FAT.seekg(FAT.beg+3);
     FAT.read(OS_name,8);
     cout << "OS NAME: " << OS_name << endl;
-    delete[] OS_name;
     /* Check FS Initalized status */
     cout << "Checking FS status..." << endl;
     if (getFATindex(2)==0){
@@ -64,18 +74,49 @@ int main(int argc, char* argv[]){
     if(status != 1){
 		cerr << "ERROR " << status << ": al ejecutar el comando" << endl;
 	}
+    delete[] OS_name;
     FAT.close();
     return 0;
 }
 
 usint getFATindex(int index){
-    cout << "wtf";
-    FAT.seekg(FAT.beg+FAToffset+(index*2));
+    FAT.seekg(FAT.beg+FAT_OFFSET+(index*2));
     usint value;
-    cout << "pls";
-    FAT.read(recast(value),2);
-    cout << " wrk :(" << endl;
+    FAT.read(recast(&value),2);
     return value;
+}
+
+void setFATindex(int index, usint value){
+    FAT.seekg(FAT.beg+FAT_OFFSET+(index*2));
+    FAT.write(rechcast(&value),2);
+}
+
+char* getDataCluster(int index){
+    FAT.seekg(FAT.beg+DATA_OFFSET+(index*CLUSTER_SIZE));
+    char* data[CLUSTER_SIZE];
+    FAT.read(data,CLUSTER_SIZE);
+    return data;
+}
+
+void setDataCluster(int index, char* data){
+    FAT.seekg(FAT.beg+DATA_OFFSET+(index*CLUSTER_SIZE));
+    FAT.write(data,CLUSTER_SIZE);
+}
+
+bool hasNextCluster(int index){
+    return getFATindex(index)!=FAT_EOF; 
+}
+
+char* getDirRawData(int index){
+
+}
+
+DirEntry* parseDirEntries(char* data){
+    DirEntry* entries = new DirEntry[512];
+    for(int i=0; i<512; i++){
+        DirEntry[i] = reinterpret_cast<DirEntry>getDirRawData(i);
+    }
+    return entries;
 }
 
 //Only works with one char delimiter but it works!!
